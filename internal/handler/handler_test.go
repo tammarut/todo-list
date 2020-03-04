69c72626f909c4c110eb5e04561773a86fa51641
tammarut <tammarut.nua@gmail.com>
2020-03-04 18:42:31 +0700

    Make a stub up

--- internal/handler/handler_test.go
+++ internal/handler/handler_test.go
@@ -6,12 +6,28 @@ import (
 	"testing"
 )
 
+type StubTaskStore struct {
+	title map[string]string
+}
+
+func (s *StubTaskStore) GetTask(taskname string) string {
+	task := s.title[taskname]
+	return task
+}
+
 func TestGETTasks(t *testing.T) {
+	store := StubTaskStore{
+		map[string]string{
+			"": "sleep",
+		},
+	}
+	handler := &TaskHandler{&store}
+
 	t.Run("returns all tasks", func(t *testing.T) {
 		request, _ := http.NewRequest(http.MethodGet, "/api/v1/tasks", nil)
 		response := httptest.NewRecorder()
 
-		TaskHandler(response, request)
+		handler.ServeHTTP(response, request)
 
 		got := response.Body.String()
 		want := "sleep"
